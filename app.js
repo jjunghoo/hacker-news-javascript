1b9fecb0007e01cd9c9acfb4714479544509f37e
jjunghoo <wjdgn2468@naver.com>
2022-07-12 01:12:37 +0900

    refactor: API 불러오는 코드 중복 개선

--- app.js
+++ app.js
@@ -4,36 +4,35 @@ const $content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
+async function getData(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
 window.addEventListener("hashchange", () => {
-  console.log("해시가 변경됨");
   const id = location.hash.substring(1);
-  console.log(id);
-
   const title = document.createElement("h1");
-  fetch(CONTENT_URL.replace("@id", id))
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      title.innerText = data.title;
 
-      $content.appendChild(title);
-    });
+  getData(CONTENT_URL.replace("@id", id)).then((data) => {
+    title.innerText = data.title;
+
+    $content.appendChild(title);
+  });
 });
 
-fetch(NEWS_URL)
-  .then((response) => response.json())
-  .then((data) => {
-    for (let i = 0; i < 10; i++) {
-      const $li = document.createElement("li");
-      const $a = document.createElement("a");
+getData(NEWS_URL).then((data) => {
+  for (let i = 0; i < 10; i++) {
+    const $li = document.createElement("li");
+    const $a = document.createElement("a");
 
-      $a.href = `#${data[i].id}`;
-      $a.innerText = `${data[i].title} (${data[i].comments_count})`;
+    $a.href = `#${data[i].id}`;
+    $a.innerText = `${data[i].title} (${data[i].comments_count})`;
 
-      $li.appendChild($a);
-      $ul.appendChild($li);
-    }
-  });
+    $li.appendChild($a);
+    $ul.appendChild($li);
+  }
+});
 
 $container.appendChild($ul);
 $container.appendChild($content);
