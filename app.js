384395d46c0ac50b5c71843a7cae7d53592263c5
jjunghoo <wjdgn2468@naver.com>
2022-07-12 19:36:41 +0900

    feat: 페이징 구현

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const $content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
+const store = {
+  currentPage: 1,
+};
+
 async function getData(url) {
   let response = await fetch(url);
   let data = await response.json();
@@ -11,14 +15,14 @@ async function getData(url) {
 }
 
 const newsDetail = () => {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
 
   getData(CONTENT_URL.replace("@id", id)).then((data) => {
     $container.innerHTML = `
       <h1>${data.title}</h1>
 
       <div>
-        <a href='#'>목록으로</a>
+        <a href='#/page/${store.currentPage}'>목록으로</a>
       </div>
     `;
   });
@@ -30,17 +34,33 @@ const newsFeed = () => {
   newsList.push("<ul>");
 
   getData(NEWS_URL).then((data) => {
-    for (let i = 0; i < 10; i++) {
+    console.log(`dataLength : ${data.length}`);
+    for (
+      let i = (store.currentPage - 1) * 10;
+      i < store.currentPage * 10;
+      i++
+    ) {
       newsList.push(`
       <li>
-        <a href='#${data[i].id}'>
+        <a href='#/show/${data[i].id}'>
           ${data[i].title} (${data[i].comments_count})
         </a>
       </li>
     `);
     }
     newsList.push("</ul>");
-
+    newsList.push(`
+      <div>
+        <a href='#/page/${
+          store.currentPage - 1 > 0 ? store.currentPage - 1 : 1
+        }'>이전 페이지</a>
+        <a href='#/page/${
+          store.currentPage + 1 > data.length / 10
+            ? store.currentPage
+            : store.currentPage + 1
+        }'>다음 페이지</a>
+      </div>
+    `);
     $container.innerHTML = newsList.join("");
   });
 };
@@ -49,6 +69,10 @@ const router = () => {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(location.hash.substring(7));
+    console.log(store.currentPage);
     newsFeed();
   } else {
     newsDetail();
