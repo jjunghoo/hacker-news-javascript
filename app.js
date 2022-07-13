f1897a5cbaaf543d6f372f3ca251512a476c479a
jjunghoo <wjdgn2468@naver.com>
2022-07-13 20:16:57 +0900

    feat: 댓글과 대댓글 가져오기

--- app.js
+++ app.js
@@ -18,13 +18,60 @@ const newsDetail = () => {
   const id = location.hash.substring(7);
 
   getData(CONTENT_URL.replace("@id", id)).then((data) => {
-    $container.innerHTML = `
-      <h1>${data.title}</h1>
+    let template = `
+      <div class="bg-gray-600 min-h-screen pb-8">
+        <div class="bg-white text-xl">
+          <div class="mx-auto px-4">
+            <div class="flex justify-between items-center py-6">
+              <div class="flex justify-start">
+                <h1 class="font-extrabold">Hacker News</h1>
+              </div>
+              <div class="items-center justify-end">
+                <a href="#/page/${store.currentPage}" class="text-gray-500">
+                  <i class="fa fa-times"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <div>
-        <a href='#/page/${store.currentPage}'>목록으로</a>
-      </div>
+        <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+          <h2>${data.title}</h2>
+          <div class="text-gray-400 h-20">
+            ${data.content}
+          </div>
+
+          {{__comments__}}
+
+        </div>
+      </div>  
     `;
+
+    const makeComments = (comments, called = 0) => {
+      const commentString = [];
+
+      for (let i = 0; i < comments.length; i++) {
+        commentString.push(`
+          <div style="padding-left: ${called * 40}px;" class="mt-4">
+            <div class="text-gray-400">
+              <i class="fa fa-sort-up mr-2"></i>
+              <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+            </div>
+            <p class="text-gray-700">${comments[i].content}</p>
+          </div>
+        `);
+
+        if (comments[i].comments.length > 0) {
+          commentString.push(makeComments(comments[i].comments, called + 1));
+        }
+      }
+      return commentString.join("");
+    };
+
+    $container.innerHTML = template.replace(
+      "{{__comments__}}",
+      makeComments(data.comments)
+    );
   });
 };
 
@@ -91,7 +138,12 @@ const newsFeed = () => {
       "{{__prev_page__}}",
       store.currentPage > 1 ? store.currentPage - 1 : 1
     );
-    template = template.replace("{{__next_page__}}", store.currentPage + 1);
+    template = template.replace(
+      "{{__next_page__}}",
+      store.currentPage + 1 > data.length / 10
+        ? store.currentPage
+        : store.currentPage + 1
+    );
     $container.innerHTML = template;
   });
 };
